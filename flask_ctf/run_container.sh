a941461ee7208139e15e2956566f08eadd76ab97
Sam Ball <stmball@gmail.com>
2024-05-21 13:06:29 +0100

    Fix conflict

--- flask_ctf/run_container.sh
+++ flask_ctf/run_container.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 docker run -it -p 2653:3000 -v "$(pwd)/secrets/wx/:/app/a" -v "$(pwd)/secrets/aw/:/app/b" treasure_hunt
-=======
-docker run -it -p 2653:3000 -v "$(pwd)/secrets/wxc/:/app/a" -v "$(pwd)/secrets/avw/:/app/b" treasure_hunt
->>>>>>> 24e512d3353d9034902d0765d6d390aacc191e47
